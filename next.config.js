26d749f5fe726891a2e2d09ee313bfdef49b82b7
--replace-all <jonathonsmcclendon@gmail.com>
2024-04-30 16:33:03 -0500

    still fixing

--- next.config.js
+++ next.config.js
@@ -4,15 +4,13 @@
  */
 await import("./src/env.js");
 
-module.exports = {
+/** @type {import("next").NextConfig} */
+const config = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
-
-/** @type {import("next").NextConfig} */
-const config = {};
 
 export default config;
