a948c3d119971824515acb160388299e4844f058
--replace-all <jonathonsmcclendon@gmail.com>
2024-04-30 16:23:31 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,14 +4,6 @@
  */
 await import("./src/env.js");
 
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-};
-
 /** @type {import("next").NextConfig} */
 const config = {};
 
